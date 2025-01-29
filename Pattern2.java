503e70f39430484f64cfa1fae1564136c3484670
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 10:24:36 +0530

    Update Pattern2.java

--- Pattern2.java
+++ Pattern2.java
@@ -1,19 +1,30 @@
 public class Pattern2 {
     public static void main(String[] args) {
-        int[][] a=new int[5][5];
-        int i,j;
-        for(i=0;i<a.length;i++){
-            for(j=0;j<a[i].length;j++){
-                if(j%2==1){
+        // Declare a 2D array 'a' with dimensions 5x5
+        int[][] a = new int[5][5];
+        
+        // Declare variables i and j for controlling the loops
+        int i, j;
+        
+        // Outer loop to iterate through rows of the array (5 rows in total)
+        for(i = 0; i < a.length; i++) {
+            // Inner loop to iterate through columns of each row (5 columns in total)
+            for(j = 0; j < a[i].length; j++) {
+                // Check if the column index 'j' is odd
+                if(j % 2 == 1) {
+                    // If the column is odd, print "#" followed by a space
                     System.out.print("# ");
                 }
-                else if(i%2==0){
+                // If the row index 'i' is even (i.e., 0, 2, 4), print "*" in even columns
+                else if(i % 2 == 0) {
                     System.out.print("* ");
                 }
-                else{
+                // Otherwise, for odd rows, print "#" in even columns
+                else {
                     System.out.print("# ");
                 }
             }
+            // Move to the next line after finishing one row
             System.out.println();
         }
     }
